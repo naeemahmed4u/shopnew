4f1d902989bed674e639841a439d8cb7ee7b96dc
naeemahmed4u <76744352+naeemahmed4u@users.noreply.github.com>
2022-02-01 17:12:32 +0500

    Initial commit

---
+++ client/src/admin/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBpVmsrUqKJkv9AM1K72DVI5v0Yf8edxL8",
+  authDomain: "big5co.firebaseapp.com",
+  projectId: "big5co",
+  storageBucket: "big5co.appspot.com",
+  messagingSenderId: "462942167788",
+  appId: "1:462942167788:web:2fff071141c2c6f2933bc9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
